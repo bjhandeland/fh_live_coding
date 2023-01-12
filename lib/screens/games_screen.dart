0a6bf4583ba5ec72814ce25447b3b3a67cd68ec1
Bjarne Handeland <bjhandeland@gmail.com>
2023-01-12 12:53:16 +0100

    updated repo

--- lib/screens/games_screen.dart
+++ lib/screens/games_screen.dart
@@ -45,7 +45,7 @@ class GamesScreen extends StatelessWidget {
           child: Column(
             children: <Widget>[
               ListTile(
-                leading: Image.asset('assets/images/gloomhaven.jpg'),
+                leading: Image.asset('assets/images/games/gloomhaven.jpg', width: 100),
                 title: const Text('Gloomhaven'),
                 subtitle:
                     const Text('Vanquish monsters with strategic cardplay. Fulfill your quest to leave your legacy!'),
@@ -66,7 +66,7 @@ class GamesScreen extends StatelessWidget {
           child: Column(
             children: <Widget>[
               ListTile(
-                leading: Image.asset('assets/images/pandemic.png'),
+                leading: Image.asset('assets/images/games/pandemic.jpg', width: 100),
                 title: const Text('Pandemic Legacy: Season 1'),
                 subtitle: const Text('Mutating diseases are spreading around the world - can your team save humanity?'),
               ),
@@ -86,7 +86,7 @@ class GamesScreen extends StatelessWidget {
           child: Column(
             children: <Widget>[
               ListTile(
-                leading: Image.asset('assets/images/brass.jpg'),
+                leading: Image.asset('assets/images/games/brass.jpg', width: 100),
                 title: const Text('Brass: Birmingham'),
                 subtitle:
                     const Text('Build networks, grow industries, and navigate the world of the Industrial Revolution.'),
@@ -107,7 +107,7 @@ class GamesScreen extends StatelessWidget {
           child: Column(
             children: <Widget>[
               ListTile(
-                leading: Image.asset('assets/images/terraforming.jpg'),
+                leading: Image.asset('assets/images/games/terraforming.jpg', width: 100),
                 title: const Text('Terraforming Mars'),
                 subtitle: const Text('Compete with rival CEOs to make Mars habitable and build your corporate empire.'),
               ),
@@ -127,7 +127,7 @@ class GamesScreen extends StatelessWidget {
           child: Column(
             children: <Widget>[
               ListTile(
-                leading: Image.asset('assets/images/twilight.jpg'),
+                leading: Image.asset('assets/images/games/twilight.jpg', width: 100),
                 title: const Text('Twilight Imperium: Fourth Edition'),
                 subtitle:
                     const Text('Build an intergalactic empire through trade, research, conquest and grand politics.'),
@@ -148,10 +148,30 @@ class GamesScreen extends StatelessWidget {
           child: Column(
             children: <Widget>[
               ListTile(
-                leading: Image.asset('assets/images/dune.jpg'),
+                leading: Image.asset('assets/images/games/dune.jpg', width: 100),
                 title: const Text('Dune'),
                 subtitle:
                     const Text('Wildly different factions vie for control of a planet through alliance and treachery.'),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  TextButton(
+                    child: const Text('More'),
+                    onPressed: () {/* ... */},
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+        Card(
+          child: Column(
+            children: <Widget>[
+              ListTile(
+                leading: Image.asset('assets/images/games/on_mars.jpg', width: 100),
+                title: const Text('On Mars'),
+                subtitle: const Text('Be a part of the first Martian colony, striving to be the best contributor.'),
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.end,
